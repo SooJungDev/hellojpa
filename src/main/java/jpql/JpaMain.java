2070e0d9fb6368f3b802a02f1b0448e1e290c554
crystal <crystal2840@gmail.com>
2022-01-05 21:32:45 +0900

    객체지향 쿼리 언어1 - 기본 문법 강의 듣는중

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,55 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * TypeQuery : 반환 타입이 명확할떄
+ * Query : 반환 타입이 명확하지 않을때
+ */
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(10);
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m  join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
