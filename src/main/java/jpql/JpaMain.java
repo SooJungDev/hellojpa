3227e0adc8c919f3cb1e5e4d1a78248d004e84c9
crystal <crystal2840@gmail.com>
2022-01-07 18:49:46 +0900

    페치조인 관련되서 강의 듣고 추가

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,11 +1,40 @@
 package jpql;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 /**
  * TypeQuery : 반환 타입이 명확할떄
  * Query : 반환 타입이 명확하지 않을때
+ * <p>
+ * 실무에서 묵시적(내부)조인 대신에 명시적 조인 사용
+ * 조인은 sql 튜닝에 중요 포인트
+ * 묵시적 조인은 조인이 일어나는 상황을 한눈에 파악하기 어려움
+ * <p>
+ * 패치 조인
+ * jpql 에서 성능 최적활르 위해 제공하는 기능
+ * 연관된 엔티티나 컬렉션은 SQL 한번에 함께 조회하는 기능
+ * join fetch 명령어 사용
+ *
+ * 패치 조인의 특징과 한계
+ *
+ * 패치 조인 대상에는 별칭을 줄 수 없다.
+ * 하이버네이트는 가능, 가급적 사용 X
+ *
+ * 둘 이상의 컬렉션은 패치 조인 할 수 없다
+ *
+ * 컬렉션을 패치 조인하면 페이징 API 를 사용 할 수 없다.
+ * 알대일, 다대일 같은 단일 값 연관 필드들은 패치 조인해도 페이징 가능
+ * 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
+ *
+ * 페치 조인의 특징과 한계
+ * 연관된 엔티티들을 SQL 한번으로 조회- 성능 최적화
+ * 엔티티에 직접 적용하는 글로벌 로딩 전략보다 우선함
+ * 실무에서 글로벌 로딩 전략은 모두 지연로딩
+ * 최적화가 필요한 곳은 페치 조인 적용
  */
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,27 +46,52 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select group_concat(m.username) from Member m";
-            List<String> result = em.createQuery(query, String.class).getResultList();
+            String query = "select t from Team t";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            System.out.println("result.size() = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println(" -> member = " + member);
+                }
+                // 회원1 ,팀A (SQL)
+                // 회원2 ,팀A (1차캐시)
+                // 회원3 ,팀B(SQL)
+
+                // 회원 100명 -> N + 1
             }
+
 
             tx.commit();
         } catch (Exception e) {
